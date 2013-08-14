ec8cf23da17f484e0dc5f8f7f55b9325f57919d4
brianpetro <brian@plexm.com>
2013-08-14 12:36:12 -0400

    subscribe response

--- app/controllers/prelaunch_controller.rb
+++ app/controllers/prelaunch_controller.rb
@@ -5,6 +5,6 @@ class PrelaunchController < ApplicationController
     api = Mailchimp::API.new("803acd89f40058142014bf89a55449c3-us6")
     email = params[:subscribe][:email]
     api.listSubscribe({id: "b1dcf737f3", email_address: email})
-    redirect_to root_path, notice: "subscribed"
+    redirect_to root_path, notice: "Almost There!"
   end
 end
