1bde37a9cddff4daa913d7becb96049f25454a82
brianpetro <brian@plexm.com>
2013-08-14 09:56:08 -0400

    set root route to prelaunch#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 DietLand::Application.routes.draw do
   get "prelaunch/home"
+  root 'prelaunch#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -40,7 +41,7 @@ DietLand::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
