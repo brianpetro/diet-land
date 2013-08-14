086cd3222cf202ab0d6abd157819d8fc84302d72
brianpetro <brian@plexm.com>
2013-08-14 09:42:40 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DietLand::Application.config.secret_key_base = '917a4e25f47847c781bbc2fb18bd16071c422af135c86be3e915c4b754ea214fd90d52016c89f0fd0f65f75f9179440f5158cf3dbf24eb81ecda56ee98d406dd'
